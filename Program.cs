e62625b5912a388309d2bd2b0d0cfb872be464b1
Stephen Brasel <stephen.brasel@gmail.com>
2017-08-21 14:08:46 -0400

    Filling out the program for Hello, World! after my first month of coding in C#

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+/// <summary>
+/// This is a simple Hello World! program. 
+/// </summary>
+public class Class1
+{
+	public void Main(string[] args)
+	{
+        Console.WriteLine("Hello World!");
+        
+        Console.SetCursorPosition(0, Console.WindowHeight - 1);
+        Console.Write("Press ENTER to continue... ");
+        Console.ReadLine();
+        Console.Clear();
+	}
+}
